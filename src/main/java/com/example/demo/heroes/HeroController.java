9ca9f75355b381d871bae79adf38f1e380033a7a
Spaceman <frederik.bjorne@gmail.com>
2018-10-20 07:17:32 +0200

    Minor misspelling in doc of hero controller

--- src/main/java/com/example/demo/heroes/HeroController.java
+++ src/main/java/com/example/demo/heroes/HeroController.java
@@ -18,7 +18,7 @@ public class HeroController {
     /**
      * Returns all heroes with short information (name, alterEgo and id) as REST endpoint /api/heroes.
      *
-     * @return An array of heroes with each heroes as a Map<key, value>.
+     * @return An array of heroes with each hero as a Map<key, value>.
      */
     @RequestMapping(method = RequestMethod.GET, value = "/api/heroes")
     Collection<Map<String, String>> getAllHeros() {
@@ -30,7 +30,7 @@ public class HeroController {
      *
      * @param id: The identifier id of a Hero as a natural number.
      * @return The heroes's detailed statistics as a Map<key, value>.
-     * @throws HeroNotFoundException if heroes is not found.
+     * @throws HeroNotFoundException if hero is not found.
      */
     @RequestMapping(method = RequestMethod.GET, value = "/api/hero/:{id}")
     Hero getHeroAllInformationById(@PathVariable Long id) throws HeroNotFoundException {
@@ -42,7 +42,7 @@ public class HeroController {
      *
      * @param shortName: The name of a Hero, e.g. "Batman". Notice that it is case sensitive.
      * @return The heroes's detailed statistics as a Map<key, value>.
-     * @throws HeroNotFoundException if heroes is not found.
+     * @throws HeroNotFoundException if hero is not found.
      */
     @RequestMapping(method = RequestMethod.GET, value = "/api/hero/{shortName}")
     Hero getHeroAllInformationByName(@PathVariable String shortName) throws HeroNotFoundException {
